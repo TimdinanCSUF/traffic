cc2ba27cae544ea161911f4f3461e3b8b8c08f52
TimdinanCSUF <70355828+TimdinanCSUF@users.noreply.github.com>
2021-03-12 13:09:15 -0800

    start

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import words from './words.svg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={words} className="App-title" alt="words" />
+        <p>
+          let us automate your social media
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          sign up
+        
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
